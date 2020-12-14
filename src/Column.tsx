a899ae35b23ae1aecedc4ee1c55848b36fca13a7
hkb <hkb@tisynch.com>
2020-12-14 12:23:05 +0100

    first commit - tasks and list addition

---
+++ src/Column.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AddNewItem } from './AddNewItem';
+import { useAppState } from './AppStateContext';
+import { Card } from './Card';
+import { ColumnContainer, ColumnTitle } from './styles';
+
+interface ColumnProps {
+    text: string,
+    index: number,
+    id: string
+}
+
+export const Column = ({ text, index, id }: ColumnProps) => {
+
+    const { state, dispatch } = useAppState();
+
+    return (
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            { state.lists[index].tasks.map((task) => {
+                return <Card text={task.text} key={task.id}/>
+            })}
+            <AddNewItem 
+                toggleButtonText="+ Add another task"
+                onAdd={text => dispatch({type: 'ADD_TASK', payload: { text, taskId: id}})}
+                dark
+            />
+        </ColumnContainer>
+
+    );
+}
+
---
+++ src/styles.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+    align-items: flex-start;
+    background-color: #3179BA;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    padding: 20px;
+    width: 100%;
+`
+
+export const ColumnContainer = styled.div`
+    background-color: #EBECF0;
+    width: 300px;
+    min-height: 40px;
+    margin-right: 20px;
+    border-radius: 3px;
+    padding: 8px;
+    flex-grow: 0;
+`
+
+export const ColumnTitle = styled.div`
+    padding: 6px 16px 12px 0;
+    font-weight: bold;
+`
+
+export const CardContainer = styled.div`
+    background-color: #FFFFFF;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    max-width: 300px;
+    border-radius: 3px;
+    box-shadow: #091E4240 0 1px 0 0; 
+`
+
+interface AddItemButtonProps {
+    dark?: boolean
+}
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+    background-color: #FFFFFF3D;
+    border-radius: 3px;
+    border: none;
+    color: ${props => (props.dark ? "#000" : "#FFF")};
+    cursor: pointer;
+    max-width: 300px;
+    padding: 10px 12px;
+    text-align: left;
+    transition: background 85ms ease-in;
+    width: 100%;
+    &:hover {
+        background-color: #FFFFFF52;
+    }
+`
+export const NewItemFormContainer = styled.div`
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: flex-start;
+`
+
+export const NewItemButton = styled.button`
+    background-color: #5AAC44;
+    border-radius: 3px;
+    border: none;
+    boy-shadow: none;
+    color: #FFF;
+    padding: 6px 12px;
+    text-align: center;
+`
+export const NewItemInput = styled.input`
+    border-radius: 3px;
+    border: none;
+    box-shadow: #091E4240 0 1px 0 0;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    width: 100%;
+`
