880488fb35b9021a5c2253ee803d55e1bcf9be1e
karl <karl@realforce.ch>
2020-12-16 20:39:34 +0100

    tasks drag and drop

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrop } from 'react-dnd';
+import { useAppState } from './AppStateContext';
+import { CardDragItem } from './DragItem';
 import { CardContainer } from './styles';
+import { useItemDrag } from './useItemDrag';
+import { isHidden } from './utils/isHidden';
+
 
 interface CardProps {
-    text: string,
-    index: number
+    isPreview?: boolean;
+    id: string;
+    text: string;
+    index: number;
+    columnId: string;
 }
 
-export const Card = ({ text }: CardProps) => {
-    return <CardContainer>{text}</CardContainer>
+export const Card = ({ id, text, columnId, index, isPreview }: CardProps) => {
+
+    const { state, dispatch } = useAppState();
+    const ref = useRef<HTMLDivElement>(null);
+    const { drag } = useItemDrag({ type: 'CARD', id, index, text, columnId });
+
+    const [, drop] = useDrop({
+        accept: 'CARD',
+        hover(item: CardDragItem) {
+
+            const dragIndex = item.index;
+            const hoverIndex = index;
+            const sourceColumn = item.columnId;
+            const targetColumn = columnId;
+
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+
+            dispatch({ type: 'MOVE_TASK', payload:{ dragIndex, hoverIndex, sourceColumn, targetColumn }});
+            item.index = hoverIndex;
+            item.columnId = targetColumn;
+        }
+    });
+
+    drag(drop(ref));
+
+    return <CardContainer
+        ref={ref}
+        isPreview={isPreview}
+        isHidden={isHidden(isPreview, state.draggedItem, 'CARD', id)}
+    >
+        {text}
+    </CardContainer>
 }
